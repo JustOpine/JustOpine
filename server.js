75db593bbe968dc89484c70027734fb17b5db28d
Lukars <lukas.siebeck@gmx.de>
2015-07-29 17:06:31 +0100

    routes and handlers

--- server.js
+++ server.js
@@ -6,19 +6,12 @@ server.connection({
 	port: Number(process.env.PORT)
 });
 
-server.route([
-  { method: 'GET', path: '/', handler: { file: "public/index.html" } },
-  // { method: 'GET', path: '/client.js', handler: { file: './client.js' } },
-  // { method: 'GET', path: '/style.css', handler: { file: './style.css' } },
-  // { method: 'GET', path: '/load',      handler: require('./lib/load_messages').load }
-]);
+server.route(require('./lib/routes.js'));
 
 server.start(function () {
 	// require('./lib/chat').init(server.listener, function(){
-	// 	console.log(process.env.REDISCLOUD_URL);
-	// 	console.log('What do you want to talk about...?', 'listening on: http://127.0.0.1:'+process.env.PORT);
 	// });
-	console.log(process.env.PORT);
+	console.log("Running on port: ", process.env.PORT);
 });
 
 module.exports = server;
