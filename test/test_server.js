30fbf6cd67cc7c09764ad5ebbf1d5ec49b99beee
Lukars <lukas.siebeck@gmx.de>
2015-07-29 19:05:00 +0100

    test with hapi inject method

--- test/test_server.js
+++ test/test_server.js
@@ -1,8 +1,16 @@
-var Shot     = require('shot');
 var assert   = require('assert');
 var server   = require('../server.js');
 
-	Shot.inject(server, { method: '', url: '/' }, function (res) {
+
+	server.inject({ method: 'GET', url: '/' }, function (res) {
 	 assert.equal(res.statusCode, '200');
 	 console.log('Status-Code: ' + res.statusCode + ' GENERIC CALL SUCCESS');
+	 server.stop(); //modularize server.js: put methods in different files and only export the ones that dont start the server
 	 });
+
+	// Shot.inject(server, { method: '', url: '/' }, function (res) {
+	//  assert.equal(res.statusCode, '200');
+	//  console.log('Status-Code: ' + res.statusCode + ' GENERIC CALL SUCCESS');
+	//  });
+ 
+
