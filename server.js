a9433c0438de8107a09c1cbf6f887f9fb18d3e2f
Mina <wilhelmina.jane@gmail.com>
2015-07-29 16:10:05 +0100

    indentation set to 4 spaces

--- server.js
+++ server.js
@@ -7,18 +7,11 @@ server.connection({
 });
 
 server.route([
-  { method: 'GET', path: '/', handler: { file: "public/index.html" } },
-  // { method: 'GET', path: '/client.js', handler: { file: './client.js' } },
-  // { method: 'GET', path: '/style.css', handler: { file: './style.css' } },
-  // { method: 'GET', path: '/load',      handler: require('./lib/load_messages').load }
+	{ method: 'GET', path: '/', handler: { file: "public/index.html" } },
 ]);
 
 server.start(function () {
-	// require('./lib/chat').init(server.listener, function(){
-	// 	console.log(process.env.REDISCLOUD_URL);
-	// 	console.log('What do you want to talk about...?', 'listening on: http://127.0.0.1:'+process.env.PORT);
-	// });
-	console.log(process.env.PORT);
+	console.log('Listening on port ' + process.env.PORT);
 });
 
 module.exports = server;
