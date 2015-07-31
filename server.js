d190f85328a13265a50677483ac0da0798f15883
Mina <wilhelmina.jane@gmail.com>
2015-07-31 11:19:40 +0100

    new db files

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-var Hapi = require('hapi');
-var server = new Hapi.Server();
-
-server.connection({
-	host: '0.0.0.0',
-	port: Number(process.env.PORT || 3000)
-});
-
-server.route(require('./lib/routes.js'));
+var server = require('./lib/server_config.js');
 
 server.start(function () {
-	// require('./lib/chat').init(server.listener, function(){
-	// });
 	console.log("Running on port: ", process.env.PORT);
 });
-
-module.exports = server;
