c3dc94f6d87fe96224f9e304337678a257d3b792
Mina <wilhelmina.jane@gmail.com>
2015-07-31 13:47:39 +0100

    sets

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var server = require('./lib/server_config.js');
+var server = require('./lib/server_config.js')();
 
 server.start(function () {
 	console.log("Running on port: ", process.env.PORT);
