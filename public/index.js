b9525a3e625a4b16f47175f169a74f9efeae7afb
msmichellegar <msmichellegar@gmail.com>
2015-08-03 12:56:16 +0100

    adds styling to student view of discussion page

--- public/index.js
+++ public/index.js
@@ -1,32 +1,20 @@
-$(document).ready(function () {
-    console.log("hey");
-});
-
-// $(".sign-in").on('click', function() {
-//     $(".landing-container-firstview").hide("slide", {direction: "left"}, 1000);
-//     $(".landing-container-secondview").show("slide", {direction: "right"}, 1000);
-// });
-
 $(".sign-in").on('click', function() {
     console.log("hey");
 
     $(".landing-container-firstview").animate({
            left: '-100%'
-       }, 500, function() {
+       }, 300, function() {
            $(".landing-container-firstview").css('left', '150%');
            $(".landing-container-firstview").appendTo('.landingcontainer');
-
-           console.log("hello");
 
            $(".landing-container-secondview").css({
                display: 'block',
                position: 'relative'
-           }, 500);
+           });
 
            $(".landing-container-secondview").animate({
                left: '0%'
-           }, 500);
+           }, 300);
        });
-
 
 });
