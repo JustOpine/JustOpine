012d8ee09b8573ba4ddebf4712af59638a500ec7
msmichellegar <msmichellegar@gmail.com>
2015-08-13 14:36:27 +0100

    assignment data now displays from redis when you use correct url

--- public/js/data.js
+++ public/js/data.js
@@ -10,8 +10,11 @@ $(document).ready(function() {
         addActionToNewPupilForm(className);
         getPupilInfo(className);
     } else if (url === "assignment1" || url === "assignment2") {
-        displayAssignmentInfo(getAssignmentInfo());
-        displayChatLogs(getChatLogs());
+        var className = window.location.href.split('/')[4];
+        var assignmentID = window.location.href.split('/')[5];
+        getAssignmentInfo(className, assignmentID);
+        // displayAssignmentInfo(getAssignmentInfo());
+        // displayChatLogs(getChatLogs());
     } else if (url === "new") {
         getClassNames(url);
     }
@@ -71,21 +74,21 @@ function addClassNamesToNewAssignmentForm(classNames) {
     }
 }
 
-function getAssignmentInfo () {
-    // hardcoded for now
-    return {
-        class: "8B",
-        title: "Should we return the Elgin Marbles?",
-        text: "At the beginning of the 19th century, Thomas Bruce, Lord Elgin, was the British Ambassador to the Ottoman Empire, which occupied Greece. He entered the Parthenon in Athens and documented the sculptures, making moulds and casts. He bribed Turkish officials to allow him to engage in daily excavations before removing a large part of the marbles to Britain. Bribing occupying powers to purloin national treasures is not the sort of behaviour usually deemed worthy of a British Ambassador. The looting that happened during the Second World War has, on the whole, been made good. No one accepts the right of those who occupied half of Europe to walk off with the revered relics of those subjugated nations in the 20th century. So why was it acceptable to do so in the 19th century? Britain missed a trick by failing to hand the marbles back during the 2012 Olympic Games, which would have been a spectacular gesture. That it did not is a sad reflection on the enfeebled spirit of philhellenism in Britain. Lord Byron, who died in Greece after travelling to fight in its war of independence, condemned the cultural vandalism of his fellow peer, Elgin. But today there are no Lord Byrons in Britain willing to raise their voice.",
-        image: "http://media-2.web.britannica.com/eb-media/22/99922-004-012C57EC.jpg",
-        category: "technology",
-        question: "Should we give back the marbles?"
-    };
+function getAssignmentInfo (className, assignmentID) {
+    var url = '/api/getAssignment/' + className + '/' + assignmentID;
+    $.ajax(url, {
+        success: function(data){
+            console.log("hello");
+            console.log(data);
+            displayAssignmentInfo(data);
+        }
+    });
 }
 
 function displayAssignmentInfo (assignment) {
     $(".opinion-piece-image").html("<img src='" + assignment.image + "'>");
     $(".opinion-piece-text").html("<h2>" + assignment.title + "</h2>" + "<p>" + assignment.text + "</p>");
+    $(".assignment-question").html("<p>" + assignment.question + "</p>");
 }
 
 function getChatLogs () {
