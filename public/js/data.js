345c48ca9a3f1c12e3afaee594cda64a9eb3047b
msmichellegar <msmichellegar@gmail.com>
2015-08-26 16:44:05 +0100

    assignments displaying with server side rendering

--- public/js/data.js
+++ public/js/data.js
@@ -7,17 +7,11 @@ $(document).ready(function() {
         addActionToNewPupilForm(className);
     } else if (url === "assignment1" || url === "assignment2") {
         var className = window.location.href.split('/')[4];
-        console.log(className);
         var assignmentID = window.location.href.split('/')[5];
         getAssignmentInfo(className, assignmentID);
         getChatLogs();
-    } else if (url === "new") {
-        getClassNames(url);
-    } else if (url === "dash1") {
-        getClassNames(url);
     }
 });
-
 
 function displayClassAsTitle (className) {
     $(".pupils-page-title").html("Pupils in " + className);
@@ -26,13 +20,6 @@ function displayClassAsTitle (className) {
 function addActionToNewPupilForm (className) {
   console.log('add action');
     $(".add-student-form").attr("action", "/api/addPupil/" + className);
-}
-
-function addClassNamesToNewAssignmentForm(classNames) {
-    for (var i = 0; i < classNames.length; i++) {
-        var name = classNames[i].split(':')[1];
-        $(".class-dropdown").append("<option value='" + name + "'>" + name + "</option>");
-    }
 }
 
 function getAssignmentInfo (className, assignmentID) {
@@ -48,19 +35,6 @@ function displayAssignmentInfo (assignment) {
     $(".opinion-piece-image").html("<img src='" + assignment.image + "'>");
     $(".opinion-piece-text").html("<h2>" + assignment.title + "</h2>" + "<p>" + assignment.text + "</p>");
     $(".assignment-question").html("<p>" + assignment.question + "</p>");
-}
-
-function getAssignmentsForAllClasses(classesArray) {
-    for (var i=0; i < classesArray.length; i++) {
-        className = classesArray[i].split(':')[1];
-        $.ajax('/api/getClassAssignments/' + className, {
-            success: function(assignments) {
-                for (var i=0; i < assignments.length; i++) {
-                    $(".dashboard-container").append('<a href="/assignment1/' + assignments[i].class + '/' + assignments[i].key.split(':')[2] + '"><div class="dashboard-assignment"><img class="class-icon" src="../static/public/images/assignment.png"><strong><p>' + assignments[i].class + '</p></strong><p>' + assignments[i].title + '</p></div></a>');
-                }
-            }
-        });
-    }
 }
 
 function getChatLogs () {
