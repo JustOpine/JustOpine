517343e11ea1c246010ec8f36bbf6c3494e9b6d0
msmichellegar <msmichellegar@gmail.com>
2015-08-27 17:55:47 +0100

    back button on landing container

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,51 @@
 // tests found in test/qunit/script-tests.js
 
-$(".sign-in").on('click', function() {
-    $(".landing-container-firstview").animate({
-           left: '-100%'
-       }, 300, function() {
-           $(".landing-container-firstview").css('left', '150%');
-           $(".landing-container-firstview").appendTo('.landingcontainer');
+function slideEffect (direction) {
+    if (direction === "right") {
+        $(".landing-container-firstview").animate({
+               left: '-100%'
+           }, 300, function() {
+               $(".landing-container-firstview").css('display', 'none');
+               $(".landing-container-firstview").appendTo('.landingcontainer');
 
-           $(".landing-container-secondview").css({
-               display: 'block',
-               position: 'relative'
+               $(".landing-container-secondview").css({
+                   display: 'block',
+                   position: 'relative'
+               });
+
+               $(".landing-container-secondview").animate({
+                   left: '0%'
+               }, 300);
            });
+    } else if (direction === "left") {
+        $(".landing-container-secondview").animate({
+               left: '100%'
+           }, 300, function() {
+               $(".landing-container-secondview").css('display', 'none');
+               $(".landing-container-secondview").appendTo('.landingcontainer');
 
-           $(".landing-container-secondview").animate({
-               left: '0%'
-           }, 300);
-       });
+               $(".landing-container-firstview").css({
+                   display: 'block',
+                   position: 'relative'
+               });
+
+               $(".landing-container-firstview").animate({
+                //    right: '-150%',
+                   left: '0%'
+               }, 300);
+           });
+    }
+}
+
+
+$(".sign-in").on('click', function() {
+    slideEffect("right");
 });
 
-// no work :(
+$(".landing-back-icon").on('click', function() {
+    slideEffect("left");
+})
+
 $(".logout").on('click', function() {
   console.log('logging out!');
     $.ajax({
@@ -39,10 +66,6 @@ function deactivateLightBox (toremove) {
     $(toremove).css("display", "none");
     $("div").first().removeClass("overlay");
 }
-
-$(".sign-in").on('click', function() {
-    slideEffect();
-});
 
 $(".add-class").on('click', function() {
     activateLightBox(".add-class-lightbox");
