a161c905555f52f757e84287843d698d11dfb54c
Mina <wilhelmina.jane@gmail.com>
2015-09-04 01:06:27 +0100

    after adding pupil, redirects back to pupil page on the server side.  renders addUser.js redundant

--- public/js/addUser.js
+++ public/js/addUser.js
@@ -1,13 +1,13 @@
-$('add-student-form').unbind();
-$('add-student-form').submit(function(e){
-  var form = $(this).serialize();
-  e.preventDefault();
-  $.ajax({
-    url: $('add-student-form').attr('action'),
-    type: 'POST',
-    data: form,
-    success: function(className){
-      location.assign("/pupils?" + className);
-    }
-  });
-});
+// $('add-student-form').unbind();
+// $('add-student-form').submit(function(e){
+//   var form = $(this).serialize();
+//   e.preventDefault();
+//   $.ajax({
+//     url: $('add-student-form').attr('action'),
+//     type: 'POST',
+//     data: form,
+//     success: function(className){
+//       location.assign("/pupils?" + className);
+//     }
+//   });
+// });
